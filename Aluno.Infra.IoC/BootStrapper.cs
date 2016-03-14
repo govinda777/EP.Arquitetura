30e57780a1cf6cb29298c55d7a2c8ffe05a05982
Luã Govinda <govinda777@gmail.com>
2016-03-14 10:38:37 -0300

    Comiti inicial

---
+++ Aluno.Infra.IoC/BootStrapper.cs
@@ -0,0 +1,42 @@
+﻿using Aluno.Application;
+using Aluno.Application.Interfaces;
+using Aluno.Data.Context;
+using Aluno.Data.Read.Context;
+using Aluno.Data.Read.Interfaces;
+using Aluno.Data.Read.Repository;
+using Aluno.Data.Repository;
+using Aluno.Domain.Core.Interfaces;
+using Aluno.Domain.Core.Services;
+using Kernel.Infra.CQRS.Repositories;
+using SimpleInjector;
+
+namespace Aluno.Infra.IoC
+{
+    public class BootStrapper
+    {
+        public static Container MyContainer { get; set; }
+
+        public static void Register(Container container)
+        {
+            // Lifestyle.Transient => Uma instancia para cada solicitacao;
+            // Lifestyle.Singleton => Uma instancia unica para a classe;
+            // Lifestyle.Scoped => Uma instancia unica para o request;
+
+            MyContainer = container;
+
+            // APP
+            container.Register<IAlunoAppService, AlunoAppService>(Lifestyle.Scoped);
+
+            // Domain
+            container.Register<IAlunoService, AlunoService>(Lifestyle.Scoped);
+
+            // Infra Dados
+            container.Register<IAlunoRepository, AlunoRepository>(Lifestyle.Scoped);
+            container.Register<IAlunoReadRepository, AlunoReadRepository>(Lifestyle.Scoped);
+
+            //container.Register<IUnitOfWork, UnitOfWork>(Lifestyle.Scoped);
+            container.Register<AlunoContext>(Lifestyle.Scoped);
+            container.Register<AlunoReadContext>(Lifestyle.Scoped);
+        }
+    }
+}
