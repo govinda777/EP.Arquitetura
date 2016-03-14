cc908a0997c1e977cdca13adcdfdd38a3fd3c54c
Luã Govinda <govinda777@gmail.com>
2016-03-14 10:38:30 -0300

    Comiti inicial

---
+++ Aluno.Infra/Handles/UserRegisteredHanle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Aluno.Domain.Core.Events;
+using Kernel.Domain.Core.Interfaces;
+
+namespace Aluno.Infra.Handles
+{
+    public class UserRegisteredHanle : IHandles<AlunoRegistrado>
+    {
+        public void Handle(AlunoRegistrado args)
+        {
+            // Enviar Email
+            Console.WriteLine("enviando email para " + args.AlunoCriado.Email);
+        }
+    }
+}
---
+++ Aluno.Infra/Utils/EmailService.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+using Aluno.Data.Context;
+using Aluno.Data.Pocos;
+
+namespace Aluno.Infra.Utils
+{
+    public class EmailService
+    {
+        public static void Send(string address, string message)
+        {
+            // TODO: Enviar Email
+
+            using (var db = new AlunoContext())
+            {
+                var email = new AlunoEmailsEnviados
+                {
+                    Address = address,
+                    Body = message,
+                    Sent = DateTime.Now
+                };
+                //db.AlunoEmailsEnviados.Add(email);
+                //db.SaveChanges();
+            }
+        }
+    }
+}
