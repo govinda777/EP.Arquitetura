5229a1a7547b4d9dc9fd2469ac26d604995c5fed
Luã Govinda <govinda777@gmail.com>
2016-03-14 10:38:13 -0300

    Comiti inicial

---
+++ Aluno.Data.Read/Context/AlunoReadContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using Aluno.Data.Read.Models;
+
+namespace Aluno.Data.Read.Context
+{
+    public class AlunoReadContext : DbContext
+    {
+        public AlunoReadContext()
+            : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<AlunoEndereco> AlunoEnderecos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlunoEndereco>()
+                .HasKey(c => c.AlunoId)
+                .ToTable("AlunoEndereco");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Aluno.Data.Read/Interfaces/IAlunoReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Aluno.Data.Read.Models;
+
+namespace Aluno.Data.Read.Interfaces
+{
+    public interface IAlunoReadRepository
+    {
+        IEnumerable<AlunoEndereco> ObterTodos();
+        AlunoEndereco ObterPorId(Guid id);
+    }
+}
